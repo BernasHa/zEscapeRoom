4431717baeffde387add2b41bd73c3526357e6e4
BernasHa <Bernas.Haydar@hotmail.de>
2023-03-07 05:03:43 +0100

    Updated Door Animation

--- zEscapeRoom/Assets/Scripts/DoorScript.cs
+++ zEscapeRoom/Assets/Scripts/DoorScript.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
+    public Animator door;
+    public GameObject openCloseText;
+    public AudioSource doorSound;
+
+    private bool inRange;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        inRange = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "InRange")
+        {
+            inRange = true;
+            openCloseText.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "InRange")
+        {
+            inRange = false;
+            openCloseText.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(inRange && Input.GetButtonDown("E"))
+        {
+            DoorOpen();
+        }
+        else
+        {
+            DoorClose();
+        }
+    }
+
+    void DoorOpen()
+    {
+        Debug.Log("Opened");
+        door.SetBool("Open", true);
+        door.SetBool("Close", false);
+        doorSound.Play();
+    }
+
+    void DoorClose()
+    {
+        Debug.Log("Closed");
+        door.SetBool("Open", false);
+        door.SetBool("Close", true);
     }
 }
